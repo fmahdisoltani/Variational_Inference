2cfe0a653e7da460a272571296fcea2f01ce8522
Farzaneh <fmahdisoltani@gmail.com>
2020-04-23 23:08:03 -0400

    Update the distributions

--- NGVI_MEF.py
+++ NGVI_MEF.py
@@ -1,134 +1,113 @@
 import torch
 import torch.nn as nn
 
-from distributions import gmm, gaussian, log_gaussian, log_gmm
+from distributions import Gaussian, GMM, NegLogGMM, gmm, gaussian
 from utils import nth_derivative
-from sampling import gaussian_sample
 from plotters import plot_posteriors, generate_animation
 from torch.distributions import Categorical
+from distributions import log_gaussian, log_gmm
+from sampling import gmm_sample
 
-class MCElbo(nn.Module):
+
+class H(nn.Module):
     def __init__(self):
-        super(MCElbo, self).__init__()
+        super().__init__()
+        # self.p = Gaussian(mean=0, sigma= 10) # prior
+        # self.q =                GMM(num_components=2, means=[-2., 2.], variances=[.5, .5], pais=[.3, .7])
+        # self.neg_log_ll = NegLogGMM(num_components=2, means=[-4., 4.], variances=[.2, .2], pais=[.7, .3])
+
+
         self.softplus = torch.nn.Softplus()
         self.softmax = torch.nn.Softmax(dim=0)
-        self.num_samples = [1]  # Number of samples
-        self.n_latent = 5  # Number of latent samples
 
         self.prior_m = torch.tensor(0.)
         self.prior_s = torch.tensor(10.)
         self.prior_fn = lambda t: log_gaussian(t, self.prior_m, self.prior_s)
 
-        self.q_means = torch.nn.Parameter(torch.tensor([-2., 2.]),
-                                          requires_grad=True)
-        self.q_stds = torch.nn.Parameter(torch.tensor([.4, .4]),
-                                         requires_grad=True)
-        self.q_log_pais = torch.nn.Parameter(torch.log(torch.tensor([.5, .5])),
-                                             requires_grad=True)
-        self.q_fn = lambda t: log_gmm(t, self.q_means,
-                                      self.softplus(self.q_stds),
-                                      self.q_log_pais)
-
-        self.likelihood_s = torch.tensor([.5, .4])
-        self.likelihood_log_pais = torch.log(torch.tensor([.5, .5]))
-        self.likelihood_mean = torch.tensor([4.])
+        self.likelihood_s = torch.tensor([.2, .2])
+        self.likelihood_log_pais = torch.log(torch.tensor([.3, .7]))
+        self.likelihood_mean = torch.tensor([-4., 4.])
 
         self.likelihood_fn = lambda t: log_gmm(t, self.likelihood_mean,
                                                self.likelihood_s,
                                                self.likelihood_log_pais)
 
-        # self.p = Gaussian(mean=0, sigma= 10) # prior
-        # self.q =                GMM(num_components=3, means=[-2.0, 0.0, 4.], variances=[5., 5., 5.], pais=[.33, .33, .33])
-        # self.neg_log_ll = NegLogGMM(num_components=3, means=[-4.,2., 5.], variances=[.2, .2, .2], pais=[.3, .5, .2])
-        # self.q =                GMM(num_components=2, means=[-2.0,  4.], variances=[5., 5.], pais=[.33, .67])
-        # self.neg_log_ll = NegLogGMM(num_components=2, means=[-4., 5.], variances=[.2,  .2], pais=[.8,  .2])
+        self.q_means = torch.tensor([-2., 2.])
+        self.q_stds = torch.tensor([.4, .4])
+        self.q_log_pais = torch.log(torch.tensor([.5, .5]))
+        self.q_fn = lambda t: log_gmm(t, self.q_means,
+                                      self.q_stds,
+                                      self.q_log_pais)
 
-    def compute_elbo(self, z):
-        q_likelihood = torch.mean(
-            log_gmm(z, self.q_means, self.softplus(self.q_stds),
-                    self.q_log_pais))
-        prior = torch.mean(self.prior_fn(z))
-        log_likelihood = torch.mean(self.likelihood_fn(z))
-
-        elbo = q_likelihood - prior - log_likelihood
-        return elbo
+    def forward(self, x):
+        h = self.q_fn(x) - self.prior_fn(x) - self.likelihood_fn(x)
+        return h
 
 
 def fit_gmm(num_components, beta, num_step=20):
-    r = 2
-    # num_step = 400
-    # num_components = 2
-    mcelbo = MCElbo()
-    # beta = 0.001
 
-    # x = torch.FloatTensor(1).uniform_(-r, r)
-    # x.requires_grad = True
+    objective = H()
+
 
     all_deltas = torch.zeros(num_components)
     grads = torch.zeros(num_components)
     hess = torch.zeros(num_components)
-    all_sampled_z = []
+    all_sampled_z = torch.zeros(num_components)
     rho = torch.zeros(num_components)
 
     all_posteriors = []
     all_normalized_posteriors = []
 
     for i in range(num_step):
-        c=0
-        sampled_z = gaussian_sample(mcelbo.q_means[c], mcelbo.softplus(mcelbo.q_stds[c]),
-                            mcelbo.num_samples[c])
 
-        # mixture_comps = Categorical(mcelbo.q_log_pais)
+        # mixture_comps = Categorical(torch.exp(objective.q_log_pais))
         # component = mixture_comps.sample()
-        # sampled_z = objective.q.means[component]
-        # sampled_z = mcelbo.q.means[c] + torch.randn(1) * torch.sqrt(mcelbo.q.variances[c])
-        # sampled_z.detach()
-        all_sampled_z.append(sampled_z)
-        dx = nth_derivative(mcelbo.compute_elbo(sampled_z), sampled_z, 1)
-        d2xx = nth_derivative(mcelbo.compute_elbo(sampled_z), sampled_z, 2)
+        # sampled_z = objective.q_means[component] + torch.randn(1) * objective.q_stds[component]
 
+        sampled_z = gmm_sample(objective.q_means, objective.q_stds, objective.q_log_pais, 1)
+        sampled_z = torch.nn.Parameter(sampled_z)
+        dx = nth_derivative(objective(sampled_z), sampled_z, 1)
+        d2xx = nth_derivative(objective(sampled_z), sampled_z, 2)
         for c in range(num_components):
-            delta = gaussian(sampled_z, mcelbo.q_means[c], mcelbo.q_stds[c]) / mcelbo.q_fn(sampled_z)
+
+            delta = gaussian(sampled_z, objective.q_means[c], objective.q_stds[c]) / torch.exp(objective.q_fn(sampled_z))
             all_deltas[c] = delta
+
             # update mean and variances and ratios
-            mcelbo.q_stds[c] = 1 / (1 / mcelbo.q_stds[c] + beta * delta * d2xx)
-            mcelbo.q_means[c] = mcelbo.q_means[c] - beta * delta * mcelbo.q_stds[c] * dx
-
-            #objective.q.variances[c] = torch.max(objective.q.variances[c], torch.FloatTensor([1e-6]))
-
-            if mcelbo.q_stds[c] < 0:
-                mcelbo.q.variances[c] = 1.0
-                print('variance reset!!!!!')
-
+            q_var = objective.q_stds[c] ** 2
+            q_var = 1 / (1 / q_var + beta * delta * d2xx)
+            objective.q_stds[c] = torch.sqrt(q_var)
+            objective.q_means[c] = objective.q_means[c] - beta * delta * q_var * dx
+            # objective.q_stds[c] = torch.sqrt(torch.max(q_var, torch.FloatTensor([1e-6])))
 
         if i % 10 == 0:
             print("STEP {} ".format(i))
             print("rhos:          {}".format(rho.data.numpy()))
-            print("q_pais:        {}".format(torch.exp(mcelbo.q_log_pais).data.numpy()))
-            print("q.means:       {}".format(mcelbo.q_means))
-            print("q.variances:      {}".format(mcelbo.q_stds))
+            print("q_pais:        {}".format(
+                torch.exp(objective.q_log_pais).data.numpy()))
+            print("q.means:       {}".format(objective.q_means))
+            print("q.stds:      {}".format(objective.q_stds))
             print("*" * 30)
-
-            beta = beta * 0.95
+            beta = beta *.95
 
         for c in range(num_components):
             # Update ratios
-            #rho[c] = rho[c] - beta * 0.01 * (all_deltas[c] - all_deltas[-1]) * objective(sampled_z)
-            weight_decay = 0.2
-            #rho[c] = (1 - weight_decay*beta)*rho[c] - beta * (all_deltas[c] - all_deltas[-1]) * objective(sampled_z)
+            rho[c] = objective.q_log_pais[c] - objective.q_log_pais[-1]
+            rho[c] = rho[c] - beta * 0.1 * (all_deltas[c] - all_deltas[-1]) * objective(sampled_z)
 
-            rho[c] = (1) * rho[c] - beta * (all_deltas[c] - all_deltas[-1]) * mcelbo.compute_elbo(sampled_z)
+        # rho = rho - torch.max(rho)
+        objective.q_log_pais = rho - torch.logsumexp(rho, axis=0, keepdims=False)
+        #torch.log(torch.softmax(rho, dim=0))
 
-        rho = rho - torch.max(rho)
-        mcelbo.q_log_pais.data = torch.softmax(rho, dim=0)
+        # grads[c] = dx
+        # hess[c] = d2xx
 
-        grads[c] = dx
-        hess[c] = d2xx
-
-        x_axis = torch.linspace(-10, 10, 500)
-        posterior = gmm(x_axis, mcelbo.q_means, mcelbo.q_stds, mcelbo.q_log_pais)
+        x_axis = torch.arange(-5, 5, 0.0001)
+        posterior = gmm(x_axis, objective.q_means, objective.q_stds, torch.exp(objective.q_log_pais))
         normalized_posterior = posterior / torch.sum(posterior)
-        true_posterior = torch.exp(-(mcelbo.likelihood_fn(x_axis))) * mcelbo.prior_fn(x_axis)
+        log_true_posterior = objective.likelihood_fn(x_axis) + objective.prior_fn(x_axis)
+
+        true_posterior = torch.exp(log_true_posterior)
         normalized_true_posterior = true_posterior / torch.sum(true_posterior)
 
         #     ax1 = fig.add_subplot(121)
@@ -143,9 +122,7 @@ def fit_gmm(num_components, beta, num_step=20):
 
 
 if __name__ == '__main__':
-
-    all_normalized_posteriors, normalized_true_posterior, x_axis = fit_gmm(num_step=200, num_components=2, beta=0.0001)
-
+    all_normalized_posteriors, normalized_true_posterior, x_axis = fit_gmm(num_step=201, num_components=2, beta=0.1)
     plot_posteriors(all_normalized_posteriors, normalized_true_posterior, x_axis)
 
-    generate_animation(all_normalized_posteriors, normalized_true_posterior, x_axis, path='./figs/gmm_3.mp4')
+    generate_animation(all_normalized_posteriors, normalized_true_posterior, x_axis,  path='./figs/gmm_3.mp4')
